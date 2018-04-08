0e4cc2f40acae18053ecfee91e6db9f5c2c1958e
Joe Davis <joecrush@msn.com>
2018-04-08 10:07:36 -0600

    Deleted files to match navpaths in mockups

--- wwwsrc/src/components/Navbar.vue
+++ wwwsrc/src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="navbar-wrapper">
-
     <nav class="navbar navbar-expand-md navbar-custom">
       <a class="navbar-brand" @click="home">
         <img src="../assets/beekeepr-nav-logo.png" class="nav-logo" alt="beekeepr">
@@ -21,7 +20,6 @@
           </li>
         </ul>
         <div class="nav-buttons-wrapper my-2 my-lg-0 ml-auto">
-
           <button class="btn btn-success signInButton" data-toggle="modal" data-target="#loginModal">Login</button>
           <button class="btn btn-primary registerButton" data-toggle="modal" data-target="#registerModal">Register</button>
           <button class="btn btn-secondary my-2 my-sm-0" @click="logout">Logout</button>
@@ -67,29 +65,7 @@
         </div>
       </div>
     </nav>
-
-
-
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/beekeepr-nav-logo.png" class="nav-logo" alt="beekeepr">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="navBtn">
-          <button class="btn btn-success" data-toggle="modal" data-target="#loginModal">Login</button>
-          <button class="btn btn-primary" data-toggle="modal" data-target="#registerModal">Register</button>
-        </div>
-      </div> -->
-
-    </nav>
   </div>
-
-
 </template>
 
 <script>
@@ -128,6 +104,11 @@
 
   .nav-logo {
     height: 80px;
+    cursor: pointer;
+  }
+
+  .nav-link {
+    cursor: pointer;
   }
 
   .navbar-custom {
